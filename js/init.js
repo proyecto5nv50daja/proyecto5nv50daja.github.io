b6b4f92098a20fdaded716c626dc47d6e9cc22dc
proyecto5nv50daja <155598570+proyecto5nv50daja@users.noreply.github.com>
2024-01-07 15:24:39 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "bdfalumnox.firebaseapp.com",
-    projectId: "bdfalumnox",
-    storageBucket: "bdfalumnox.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
+    apiKey: "AIzaSyDqR658Q8_Ab8cfCgYP5RRwvNR0ne7z7Vs",
+  authDomain: "proyecto5nv50daja-2f527.firebaseapp.com",
+  projectId: "proyecto5nv50daja-2f527",
+  storageBucket: "proyecto5nv50daja-2f527.appspot.com",
+  messagingSenderId: "765224830676",
+  appId: "1:765224830676:web:4eda9b6c528aa96a4b1d41",
+  measurementId: "G-W6NF9R2YLJ"
 });
