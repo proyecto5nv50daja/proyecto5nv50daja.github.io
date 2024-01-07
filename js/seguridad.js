2d47932cc49ddee790129576418d565e1f457dfc
proyecto5nv50daja <155598570+proyecto5nv50daja@users.noreply.github.com>
2024-01-07 16:38:19 -0600

    Update seguridad.js

--- js/seguridad.js
+++ js/seguridad.js
@@ -25,7 +25,23 @@ export async function iniciaSesión() {
   await getAuth().
     signInWithRedirect(provider);
 }
-
+export async function iniciaSesión() {
+  /** Tipo de autenticación de
+   * usuarios. En este caso es con
+   * Google.
+   * @type {import("../lib/tiposFiregoogle.js").GoogleAuthProvider} */
+  const provider =
+    // @ts-ignore
+    new firebase.auth.
+      GoogleAuthProvider();
+  /* Configura el proveedor de
+   * Google para que permita
+   * seleccionar de una lista. */
+  provider.setCustomParameters(
+    { prompt: "select_account" });
+  await getAuth().
+    signInWithRedirect(provider);
+}
 /** @param {import(
     "../lib/tiposFire.js").User}
     usuario
