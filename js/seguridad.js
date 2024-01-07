cd6efe402ae9b2ca4a42136d118007c072112ece
proyecto5nv50daja <155598570+proyecto5nv50daja@users.noreply.github.com>
2024-01-07 15:36:46 -0600

    Update seguridad.js

--- js/seguridad.js
+++ js/seguridad.js
@@ -13,7 +13,7 @@ export async function iniciaSesión() {
   /** Tipo de autenticación de
    * usuarios. En este caso es con
    * Google.
-   * @type {import("../lib/tiposFire.js").GoogleAuthProvider} */
+   * @type {import("../lib/tiposFire.js").FacebookAuthProvider} */
   const provider =
     // @ts-ignore
     new firebase.auth.
